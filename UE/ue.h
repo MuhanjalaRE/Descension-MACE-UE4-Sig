818aec305c1ca632aa61271cb44f97d1435bab28
MuhanjalaRE <78203566+MuhanjalaRE@users.noreply.github.com>
2022-07-03 17:54:26 +1000

    Initial commit
    Initial commit

---
+++ UE/ue.h
@@ -0,0 +1,13 @@
+#pragma once
+namespace ue {
+extern bool frame_is_ready;
+extern const char* ufunction_to_hook;
+
+void HookUnrealEngine4(void);
+void DrawImGuiInUE4(void);
+void UE4_(void);
+}
+
+namespace aimbot {
+void Reset(void);
+}
